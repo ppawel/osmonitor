62b54042237dbccb07143fbafdc4848c804fa64d
Paweł Paprota <ppawel@fastmail.fm>
2012-08-12 12:11:54 +0200

    Indexes ftw

---
+++ sql/osmonitor_idx.sql
@@ -0,0 +1,29 @@
+DROP INDEX IF EXISTS idx_relation_members_member_id;
+CREATE INDEX idx_relation_members_member_id
+  ON relation_members
+  USING btree
+  (member_id );
+
+DROP INDEX IF EXISTS idx_relation_members_relation_id;
+CREATE INDEX idx_relation_members_relation_id
+  ON relation_members
+  USING btree
+  (relation_id );
+
+DROP INDEX IF EXISTS idx_way_nodes_node_id;
+CREATE INDEX idx_way_nodes_node_id
+  ON way_nodes
+  USING btree
+  (node_id );
+
+DROP INDEX IF EXISTS idx_way_nodes_way_id;
+CREATE INDEX idx_way_nodes_way_id
+  ON way_nodes
+  USING btree
+  (way_id );
+
+DROP INDEX IF EXISTS idx_ways_refs;
+CREATE INDEX idx_ways_refs
+  ON ways
+  USING gin
+  (refs);
