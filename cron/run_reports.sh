f309cd3811eb62dc9bd615fea6436a1d5b1236eb
Paweł Paprota <ppawel@fastmail.fm>
2012-08-27 20:54:00 +0200

    Resolve some working dir problems.

--- cron/run_reports.sh
+++ cron/run_reports.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-ruby /home/ppawel/osmonitor/src/run_road_report.rb "OSMonitor/Poland_Major_Roads" &> ~/log/report_poland_major_roads.log &
-ruby /home/ppawel/osmonitor/src/run_road_report.rb "OSMonitor/Poland_Regional_Roads" &> ~/log/report_poland_regional_roads.log &
+cd /home/ppawel/osmonitor/src
+
+ruby run_road_report.rb "OSMonitor/Poland_Major_Roads" &> ~/log/report_poland_major_roads.log &
+ruby run_road_report.rb "OSMonitor/Poland_Regional_Roads" &> ~/log/report_poland_regional_roads.log &
