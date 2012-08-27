e24bfb0ee2782adf78e28eb9826962a23d4260a0
Paweł Paprota <ppawel@fastmail.fm>
2012-08-27 20:50:32 +0200

    Additional script for running reports.

---
+++ cron/run_reports.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ruby /home/ppawel/osmonitor/bin/run_road_report.rb "OSMonitor/Poland_Major_Roads" &> ~/log/report_poland_major_roads.log &
+ruby /home/ppawel/osmonitor/bin/run_road_report.rb "OSMonitor/Poland_Regional_Roads" &> ~/log/report_poland_regional_roads.log &
