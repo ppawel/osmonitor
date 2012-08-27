f5b4b484f6f075c063cef557b028f80223f3b57c
Paweł Paprota <ppawel@fastmail.fm>
2012-08-27 20:52:48 +0200

    bin -> src

--- cron/run_reports.sh
+++ cron/run_reports.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ruby /home/ppawel/osmonitor/bin/run_road_report.rb "OSMonitor/Poland_Major_Roads" &> ~/log/report_poland_major_roads.log &
-ruby /home/ppawel/osmonitor/bin/run_road_report.rb "OSMonitor/Poland_Regional_Roads" &> ~/log/report_poland_regional_roads.log &
+ruby /home/ppawel/osmonitor/src/run_road_report.rb "OSMonitor/Poland_Major_Roads" &> ~/log/report_poland_major_roads.log &
+ruby /home/ppawel/osmonitor/src/run_road_report.rb "OSMonitor/Poland_Regional_Roads" &> ~/log/report_poland_regional_roads.log &
