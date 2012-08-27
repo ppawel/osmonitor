abb50a729a1dca05513c2d3deca653d7f6f07467
Paweł Paprota <ppawel@fastmail.fm>
2012-08-27 17:30:41 +0200

    Cron

--- cron/run_replication.sh
+++ cron/run_replication.sh
@@ -10,6 +10,6 @@ set -e
   # Try to lock on the lock file (fd 200)
   flock -x -n 200
 
-  /home/ppawel/osmonitor/bin/replication_update.sh &> ~/replication.log
+  /home/ppawel/osmonitor/bin/replication_update.sh &>> ~/replication.log
 
 ) 200>/var/lock/osmonitor_replication.lock
