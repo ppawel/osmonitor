f0fb348a8ce64943c6b4e201c63336a5d94a486e
Paweł Paprota <ppawel@fastmail.fm>
2012-08-27 21:47:38 +0200

    Run preprocessing after replication

--- cron/run_replication.sh
+++ cron/run_replication.sh
@@ -11,5 +11,6 @@ set -e
   flock -x -n 200
 
   /home/ppawel/osmonitor/bin/replication_update.sh &>> ~/log/replication.log
+  /home/ppawel/osmonitor/bin/preprocess_db.sh &>> ~/log/replication.log
 
 ) 200>/var/lock/osmonitor_replication.lock
