bf553743d052119b5a3363c869b668fd83ac8312
Paweł Paprota <ppawel@fastmail.fm>
2012-09-06 19:52:59 +0200

    Export LANG env variable with UTF8 before running the reports.

--- cron/run_road_report.sh
+++ cron/run_road_report.sh
@@ -3,6 +3,9 @@
 # Add Ruby executable to the PATH.
 export PATH=/home/ppawel/.rvm/bin:$PATH
 
+# Avoid problems with language specific strings.
+export LANG="en_US.utf8"
+
 cd /home/ppawel/osmonitor/src
 
 ruby run_road_report.rb $1 &> $2
