562ba9c957adc5f7a90a16ec210d088df01b71ab
Paweł Paprota <ppawel@fastmail.fm>
2012-09-14 00:01:50 +0200

    Admin report validations.

--- src/osmonitor/admin_report/report_model.rb
+++ src/osmonitor/admin_report/report_model.rb
@@ -4,7 +4,20 @@ module AdminReport
 class BoundaryStatus < OSMonitor::Status
   def validate
     add_error('no_relation') if !@entity.relation
-    add_error('boundary_not_closed') if @entity.relation and !@entity.closed
+
+    if @entity.relation
+      add_error('boundary_not_closed') if !@entity.closed
+      add_error('boundary_admin_level') if !correct_admin_level?
+      add_error('boundary_teryt') if !correct_teryt_id?
+    end
+  end
+
+  def correct_admin_level?
+    @entity.relation.tags['admin_level'] == @entity.input['admin_level']
+  end
+
+  def correct_teryt_id?
+    @entity.relation.tags['teryt:terc'] == @entity.input['id']
   end
 end
 
